74dc3608b3cc16df91d77dd54f5103175d0b426a
Mohamed Hamzeh <hamzehm@uwindsor.ca>
2026-05-27 16:23:14 -0400

    Add MPC parameters block : cost weights, constraint limits, lateral acceleration threshold

--- mpc_controller.py
+++ mpc_controller.py
@@ -23,13 +23,30 @@ curvature_raw = np.abs((dx[:-1] * ddy - dy[:-1] * ddx) / (dx[:-1]**2 + dy[:-1]**
 curvature = np.append(curvature_raw, [curvature_raw[-1], curvature_raw[-1]])
 
 max_speed = 30.0
-k_curve = 500 #curvature constant
+k_curve = 500 
 speed_ref = max_speed/ (1 + k_curve * curvature)
 curvature = np.convolve(curvature, np.ones(15)/15, mode='same') 
 
+# --- MPC Parameters ---
 
 # plt.plot(curvature)
 # plt.title('Curvature along reference path')
 # plt.xlabel('Waypoint index')
 # plt.ylabel('Curvature')
 # plt.show()
+
+
+Q1 = 10.0 
+Q2 = 5.0
+Q3 = 1.0
+R1 = 0.1
+R2 = 0.1
+R3 = 0.5
+R4 = 0.5
+
+delta_max = np.radians(28)
+a_max = 3.0
+v_min = 2.0
+v_max = max_speed
+k_speed = 0.05
+lat_accel_max = 2.943 / 0.221 #0.3g (multiplied by 9.81) - Limit for comfortable lateral acceleration
