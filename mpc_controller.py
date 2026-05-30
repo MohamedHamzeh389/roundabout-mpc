fcc0074527b56ca71ed8ec8394d23bd7d598f419
Mohamed Hamzeh <hamzehm@uwindsor.ca>
2026-05-30 11:36:27 -0400

    Complete mpc_solve function:  cost, constraints, OSQP solver

--- mpc_controller.py
+++ mpc_controller.py
@@ -95,9 +95,37 @@ def mpc_solve(state, waypoint_idx):
                 )
 
 
-             
-    
-    
     constraints = []
-    
-    return None  
+
+    constraints += [states[:, 0] == state]
+
+    for t in range(N):
+        idx = (waypoint_idx + t) % 500
+        ref_state = np.array([x[idx], y[idx], heading[idx], speed_ref[idx]])
+        A, B = get_linear_model(ref_state, 0.0)
+        constraints += [states[:, t+1] == A @ states[:, t] + B @ inputs[:, t]]
+
+    constraints += [cp.abs(inputs[0, :]) <= delta_max]
+    constraints += [cp.abs(inputs[1, :]) <= a_max]
+        
+    constraints += [states[3, :] >= v_min] 
+    constraints += [states[3, :] <= v_max]
+
+    for t in range(N):
+        idx = (waypoint_idx + t) % 500
+        v_ref_t = speed_ref[idx]
+        steer_limit = delta_max / (1 + k_speed * v_ref_t)
+        constraints += [cp.abs(inputs[0, t]) <= steer_limit]
+
+    for t in range(N):
+        idx = (waypoint_idx + t) % 500
+        kappa = curvature[idx]
+        if kappa > 1e-6:
+            max_v = np.sqrt(lat_accel_max / kappa)
+            constraints += [states[3, t] <= max_v]
+            
+    problem = cp.Problem(cp.Minimize(cost), constraints)
+
+    problem.solve(solver=cp.OSQP, warm_start=True)
+
+    return inputs[:,0].value
